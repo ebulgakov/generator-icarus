fcffb9cae1a3a748b50afff4a9f0332ac39ae31a
Evgeniy Bulgakov <bulgakov@dominion.ru>
2014-11-18 00:15:59 +0400

    Run One Version

--- app/index.js
+++ app/index.js
@@ -10,7 +10,6 @@ var IcarusGenerator = module.exports = function IcarusGenerator (args, options, 
 	yeoman.generators.Base.apply(this, arguments);
 
 	this.indexFile = this.readFileAsString(path.join(this.sourceRoot(), 'main.html'));
-	this.mainJsFile = '';
 
 	this.on('end', function () {
 
@@ -111,15 +110,18 @@ IcarusGenerator.prototype.mainStylesheet = function mainStylesheet() {
 	deps += '@img-path: "../img/d/";\n';
 
 	if (this.fontawesome) {
-		deps += '@icon-font-path: "../fonts/glyphicons/";\n';
+		deps += '@fa-font-path: "../fonts/font-awesome";\n';
+		bsPath += '@import "../bower_components/font-awesome/less/font-awesome";\n';
 	}
 
 	if (this.bootstrap) {
+		deps += '@icon-font-path: "../fonts/glyphicons/";\n';
 		bsPath += '@import "../bower_components/bootstrap/less/bootstrap";\n@import "bootstrap/variables";\n';
 		this.write('app/css/bootstrap/variables.less', deps);
 	} else {
-		bsPath = '@import (css) "../bower_components/normalize-css/normalize.css";\n@import "system/bootstrap";\n';
+		bsPath += '@import (css) "../bower_components/normalize-css/normalize.css";\n@import "system/bootstrap";\n';
 		this.write('app/css/system/variables.less', deps);
+
 		var customBs = '@import "variables";\n@import "mixins";\n@import "structure";\n@import "type";\n@import "utilities";\n';
 		this.write('app/css/system/bootstrap.less', customBs);
 	}
@@ -134,40 +136,17 @@ IcarusGenerator.prototype.writeIndex = function writeIndex() {
 	var contentText = [
 		'    <div class="container">',
 		'      <div class="jumbotron">',
-		'        <h1>\'Allo, \'Allo!</h1>',
+		'        <h1><i class="icon-yeoman"></i> \'Allo, \'Allo!</h1>',
 		'        <p>You now have</p>',
 		'        <ul>'
 	];
 
-	this.indexFile = this.appendScripts(this.indexFile, 'js/main.js', [
-		'bower_components/jquery/jquery.js',
-		'js/main.js'
-	]);
 
-
-	if (this.bootstrap) {
-		// wire Bootstrap plugins
-		this.indexFile = this.appendScripts(this.indexFile, 'js/vendor/bootstrap.js', [
-			'bower_components/bootstrap/js/affix.js',
-			'bower_components/bootstrap/js/alert.js',
-			'bower_components/bootstrap/js/dropdown.js',
-			'bower_components/bootstrap/js/tooltip.js',
-			'bower_components/bootstrap/js/modal.js',
-			'bower_components/bootstrap/js/transition.js',
-			'bower_components/bootstrap/js/button.js',
-			'bower_components/bootstrap/js/popover.js',
-			'bower_components/bootstrap/js/carousel.js',
-			'bower_components/bootstrap/js/scrollspy.js',
-			'bower_components/bootstrap/js/collapse.js',
-			'bower_components/bootstrap/js/tab.js'
-		]);
-	}
 
 	if (this.fontawesome) {
 		defaults.push('Font Awesome <i class="fa fa-flag"></i>');
 	}
 
-	this.mainJsFile = 'console.log(\'Hello World!\');';
 
 	// iterate over defaults and create content string
 	defaults.forEach(function (el) {
@@ -184,14 +163,13 @@ IcarusGenerator.prototype.writeIndex = function writeIndex() {
 	]);
 
 	// append the default content
-	this.indexFile = this.indexFile.replace('<body>', '<body>\n' + contentText.join('\n'));
+	this.indexFile = this.indexFile.replace('@@include("include/header.html")', '@@include("include/header.html")\n' + contentText.join('\n'));
+
 };
 
 IcarusGenerator.prototype.app = function app() {
 	this.directory('app');
 	this.write('app/main.html', this.indexFile);
-	this.write('app/js/main.js', this.mainJsFile);
-
 
 	if (!this.bootstrap) {
 		this.write('app/css/system/mixins.less', '/* Mixins */\n\n');
